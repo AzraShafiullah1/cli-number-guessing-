05ec910b9ce6c153aa615be7fdda9116d363e98b
AzraShafiullah1 <164544626+AzraShafiullah1@users.noreply.github.com>
2024-03-27 01:37:12 +0500

    push my calculator to github

---
+++ index.ts
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+// 1) computer will generate a random number - Done
+
+// 2) user input for gussing number - Done
+
+// 3) compare user input with computer generated number and show result - Done
+
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "please guess a number between 1-10:",
+    },
+]);
+
+if (answer.userGuessedNumber === randomNumber) {
+    console.log("congratulations! you guessed right number.");
+} else {
+    console.log("you guess wrong number");
+}
